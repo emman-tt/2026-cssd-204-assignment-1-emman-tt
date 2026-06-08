30032e84825c676eeccc2f698b4836e2559ffe3d
Emmanuel Acquah <emmanuelaquarius2006@gmail.com>
2026-06-08 01:34:13 +0000

    initials assignment  commit

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
 
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Emmanuel Acquah
+# STUDENT ID:  2425401371
+# Email: 2425401371@live.gctu.edu.gh
+# Grader: Emma
+#
+# On my honor, Emmanuel, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+ 
 def main():
-  pass #code goes here
-
-
-
+    # Print my initials in stylized large letters using the print function and strings only
+    print()
+    print("...EJA")
+    print()
+    print("...EEEEEEEEEEEE.......JJJJJJJJJJJJ...........AAAA........")
+    print("...EE.........................JJ............AA..AA.......")
+    print("...EE.........................JJ...........AA....AA......")
+    print("...EE.........................JJ..........AA......AA.....")
+    print("...EEEEEEEEE..................JJ.........AA........AA....")
+    print("...EE.........................JJ.........AAAAAAAAAAAA....")
+    print("...EE.........................JJ.........AA........AA....")
+    print("...EE.................JJ......JJ.........AA........AA....")
+    print("...EE............**....JJJJJJJJ.....**...AA........AA..**")
+    print("...EEEEEEEEEEEE..**.....JJJJJJ......**...AA........AA..**")
+    print()
+ 
+ 
 main()
